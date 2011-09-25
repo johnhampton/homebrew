9b800b2509e3f0a91471fbfa1761663bea80cdda
John Hampton <john@topagentnetwork.com>
2011-09-25 13:01:56 -0700

    Added dependency on gnu-sed

--- Library/Formula/knife-bash-completion.rb
+++ Library/Formula/knife-bash-completion.rb
@@ -4,6 +4,8 @@ class KnifeBashCompletion < Formula
   homepage 'https://gist.github.com/1050685'
   head 'git://gist.github.com/1050685.git'
 
+  depends_on 'gnu-sed'
+
   def install
     (prefix+'etc/bash_completion.d').install 'knife'
   end
